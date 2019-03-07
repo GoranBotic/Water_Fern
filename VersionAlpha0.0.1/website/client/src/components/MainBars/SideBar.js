be9b20efe3c4f0da91986cc2f4f4459150c3155a
Patrick <patrickfbright@gmail.com>
2019-03-07 16:49:56 -0500

    prepping for switch to JS

--- VersionAlpha0.0.1/website/client/src/components/MainBars/SideBar.js
+++ VersionAlpha0.0.1/website/client/src/components/MainBars/SideBar.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import './sidebar.css';
 
+function choose(elem){
+   elem.title = "new thing"
+
+}
+
 class SideBar extends Component {
 
    render() {
-
+      
       const list = [
-         {"title": "COSC 1P02"},
-         {"title": "MATH 1P05"},
-         {"title": "MATH 1P66"},
-         {"title": "MEME 1P69"}
+         {
+            "id": 1,
+            "title": "COSC 1P02"},
+         {
+            "id": 2,
+            "title": "MATH 1P05"},
+         {
+            "id": 3,
+            "title": "MATH 1P66"},
+         {
+            "id": 4,
+            "title": "MEME 1P69"}
       ]
 
       const li = list.map(function (elem) {
-         return <li className="selection-list">{elem.title}</li>
+         return <li className="selection-list" onClick={choose(elem)}> {elem.title}</li>
       })
 
       return (
